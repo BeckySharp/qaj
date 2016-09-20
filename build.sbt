7817913c6bcad7c5f69af1304e23bc8a500a0f56
Rebecca Sharp <bsharp@cottonwood.cs.arizona.edu>
2016-09-19 17:10:37 -0700

    starting to build the indexer

--- build.sbt
+++ build.sbt
@@ -2,4 +2,11 @@ name := "qaj"
 
 version := "1-0-SNAPSHOT"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.clulab" %% "processors-corenlp" % "6.0.0",
+  "org.clulab" %% "processors-main" % "6.0.0",
+  "org.clulab" %% "processors-models" % "6.0.0",
+  "org.apache.lucene" % "lucene-core" % "6.2.0"
+)
--- src/main/scala/indexing/PassageGenerator.scala
+++ src/main/scala/indexing/PassageGenerator.scala
@@ -1,8 +1,44 @@
 package indexing
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by bsharp on 9/19/16.
+  * Classes for dividing knowledge sources into passages for indexing
   */
 trait PassageGenerator {
+  var nPassagesSeen: Int = 0
+  val passageTypePrefix:String
+  def generatePassageId(): String = s"$passageTypePrefix$nPassagesSeen"
+  def divideFileText (filename:String): Seq[Passage]
+}
 
+class SentenceGenerator() extends PassageGenerator {
+
+  val passageTypePrefix = "Sent"
+
+  def divideFileText (fn: String): Seq[Passage] = {
+    val out = new ArrayBuffer[Passage]
+
+    // Open the file
+    val source = scala.io.Source.fromFile(fn)
+    val lines = source.getLines().toArray
+
+    // Process the text
+    for (line <- lines) {
+      // Process the line (this is really a placeholder for more complex processing...
+      val sentence = line.trim()
+
+      // Add the passage to the buffer
+      out.append(new Passage(generatePassageId(), sentence))
+
+      // Increment the nPassagesSeen to serve as a passageID for indexing
+      nPassagesSeen += 1
+    }
+
+    // Housekeeping
+    source.close()
+
+    out.toSeq
+  }
 }
